c9a92a39ff00580bedd59ba987d8c7f0be451d3d
Isra <isgonfer@gmail.com>
2020-05-29 18:57:14 +0200

    First commit of the Primeras Vistas app designed to learn Android layouts

---
+++ app/src/main/java/com/example/primerasvistas/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.primerasvistas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        //setContentView(R.layout.constraint)
+        //setContentView(R.layout.constraint_layout_practice2)
+        //setContentView(R.layout.table_layout)
+        //setContentView(R.layout.linear_layout)
+        setContentView(R.layout.constraint_layout_3)
+    }
+}
